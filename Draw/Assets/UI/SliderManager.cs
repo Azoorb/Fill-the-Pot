2d933d63e888fcefc701659c11d7f37961e463ec
Azoorb <leo.dornaz@gmail.com>
2020-06-16 22:16:09 +0200

    V1

---
+++ Draw/Assets/UI/SliderManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderManager : MonoBehaviour
+{
+    public static SliderManager instance;
+    [SerializeField]
+    Slider sliderLine,sliderWater;
+    [SerializeField]
+    Image fillLine,fillWater;
+    [SerializeField]
+    Gradient gradientLine;
+    private void Awake()
+    {
+        if(instance ==null)
+        {
+            instance = this;
+        }
+
+    }
+
+    private void Start()
+    {
+        sliderLine.maxValue = DrawManager.instance.amountLine;
+        sliderLine.minValue = 0;
+        SetSliderLine(DrawManager.instance.amountLine);
+        sliderWater.maxValue = PaddleScript.instance.totalWater;
+        sliderWater.minValue = 0;
+        SetSliderWater(PaddleScript.instance.totalWater);
+    }
+
+    public void SetSliderLine(int value)
+    {
+        sliderLine.value = value;
+        fillLine.color = gradientLine.Evaluate(sliderLine.normalizedValue);
+    }
+
+    public void SetSliderWater(int value)
+    {
+        sliderWater.value = value;
+    }
+
+
+}
---
+++ Draw/Assets/bucket/BucketScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BucketScript : MonoBehaviour
+{
+    [SerializeField] int pointForWin;
+    int actualPointNumber;
+
+    private void Start()
+    {
+        actualPointNumber = pointForWin;
+    }
+    
+
+    public void ReducePoint()
+    {
+        actualPointNumber--;
+        if(actualPointNumber == 0)
+        {
+            Debug.Log("Win ! ");
+        }
+    }
+}
